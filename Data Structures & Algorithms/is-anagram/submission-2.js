4d36de8cef5e3214641f32ea7db5aae305532a70
KennethVelazquez <87355077+nadielotiene@users.noreply.github.com>
2026-05-05 13:59:25 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+
+
+        let sortedS = s.split('').sort().join('');
+        let sortedT = t.split('').sort().join('');
+
+        return sortedS === sortedT;
+    }
+}
