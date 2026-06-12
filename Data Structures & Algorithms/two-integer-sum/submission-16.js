dfb363c32496a95d0db5f7092c0b5847f3702a3d
KennethVelazquez <87355077+nadielotiene@users.noreply.github.com>
2026-06-12 18:25:53 -0400

    Add: two-integer-sum - submission-16

---
+++ Data Structures & Algorithms/two-integer-sum/submission-16.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let A = [];
+        for (let i = 0; i < nums.length; i++) {
+            A.push([nums[i], i]);
+        }
+
+        A.sort((a, b) => a[0] - b[0]);
+
+        let i = 0,
+            j = nums.length - 1;
+        while (i < j) {
+            let cur = A[i][0] + A[j][0];
+            if (cur === target) {
+                return [Math.min(A[i][1], A[j][1]), Math.max(A[i][1], A[j][1])];
+            } else if (cur < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return [];
+    }
+}
