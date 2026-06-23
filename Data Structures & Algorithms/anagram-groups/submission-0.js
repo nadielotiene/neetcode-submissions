df4bd7fb2ea4ff3ba5625be91996135852fe199e
KennethVelazquez <87355077+nadielotiene@users.noreply.github.com>
2026-06-23 13:54:49 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map();
+
+        for (let str of strs) {
+            const sortedKey = str.split('').sort().join('');
+            if (!map.has(sortedKey)) {
+                map.set(sortedKey, [])
+            }
+            map.get(sortedKey).push(str);
+        }
+        return Array.from(map.values());
+    }
+}
